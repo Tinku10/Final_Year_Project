dcefc3a78fba147dfade555f71aaa4bbd4e83185
Tinku10 <tinku.kvs@gmail.com>
2019-11-28 01:24:04 +0530

    Update distance_to_camera.py

--- distance_to_camera.py
+++ distance_to_camera.py
@@ -4,23 +4,20 @@ import imutils
 import cv2
 
 
+
+
 def find_marker(image):
 	# convert the image to grayscale, blur it, and detect edges
+	
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (5, 5), 0)
-	edged = cv2.Canny(gray, 30, 125)
-	#edged = imutils.auto_canny(gray) // Bad edge detection
-	#cv2.imshow('win', edged)
+	edged = cv2.Canny(gray, 35, 125)
 	
-	
+ 
 	# find the contours in the edged image and keep the largest one;
 	# we'll assume that this is our piece of paper in the image
 	cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-	print(cnts)
-	print("cxcvvbcbbnvnbnmhhkjhxjhlolhxckgfzdsdcSXAX")
 	cnts = imutils.grab_contours(cnts)
-	print(cnts)
-	
 	c = max(cnts, key = cv2.contourArea)
  
 	# compute the bounding box of the of the paper region and return it
@@ -28,9 +25,13 @@ def find_marker(image):
 
 
 
+
+
 def distance_to_camera(knownWidth, focalLength, perWidth):
 	# compute and return the distance from the maker to the camera
 	return (knownWidth * focalLength) / perWidth
+
+
 
 
 #Find distance from camera to object using Python and OpenCVPython
@@ -44,25 +45,25 @@ KNOWN_WIDTH = 11.0
 
 path = 'd:\Personal Files\Project_Final\\test_images\img1.png'
 image = cv2.imread(path)
-# cv2.imshow('img', image)
+cv2.imshow('img', image)
 marker = find_marker(image)
 focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 
 # loop over the images
-# for imagePath in sorted(paths.list_images("d:\Personal Files\Project_Final\\test_images")):
-# 	# load the image, find the marker in the image, then compute the
-# 	# distance to the marker from the camera
-# 	image = cv2.imread(imagePath)
-# 	marker = find_marker(image)
-# 	inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+for imagePath in sorted(paths.list_images("d:\Personal Files\Project_Final\\test_images")):
+	# load the image, find the marker in the image, then compute the
+	# distance to the marker from the camera
+	image = cv2.imread(imagePath)
+	marker = find_marker(image)
+	inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
  
-# 	# draw a bounding box around the image and display it
-# 	box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
-# 	box = np.int0(box)
-# 	cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-# 	cv2.putText(image, "%.2fcm" % (inches*2.54),
-# 		(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-# 		2.0, (0, 255, 0), 3)
-# 	#cv2.imshow("image", image)
-# 	cv2.waitKey(0)
+	# draw a bounding box around the image and display it
+	box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+	box = np.int0(box)
+	cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+	cv2.putText(image, "%.2fcm" % (inches*2.54),
+		(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+		2.0, (0, 255, 0), 3)
+	cv2.imshow("image", image)
+	cv2.waitKey(0)
